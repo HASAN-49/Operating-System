23de97ed554f69339e4c01845e710e52f2df1f06
Mahmudul Hasan Bhuiyan <107288719+HASAN-49@users.noreply.github.com>
2025-07-13 20:05:28 +0600

    Add files via upload

---
+++ question15/two.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/shm.h>
+
+int main() {
+    key_t key = 1234;
+    
+    int shmid = shmget(key, sizeof (int), IPC_CREAT | 0666);
+    if (shmid == -1) {
+        perror("shmget");
+        exit(1);
+    }
+    
+    int *var = shmat(shmid, NULL, 0);
+    if (var == (int *) -1) {
+        perror("shmat");
+        exit(1);
+    }
+    
+    for (int i = 0; i < 10; i++) {
+        printf("Before write: %d\n", *var);
+        *var = 999;
+        printf("After write: %d\n", *var);
+        usleep(100000); // 100 ms
+    }
+    
+    if (shmdt(var) == -1) {
+        perror("shmdt");
+        exit(1);
+    }
+    
+    shmctl(shmid, IPC_RMID, NULL);
+    
+    exit(EXIT_SUCCESS);
+}
